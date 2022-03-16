0291b38c757a6ac82bff7c2d4ab44de85445299b
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-03-16 09:56:36 +0300

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 /*________ CODEWARS__________ */
+/*
+Complete the function that takes a non-negative integer n as input, 
+and returns a list of all the powers of 2 with the 
+exponent ranging from 0 to n ( inclusive ).*/
+
+const powersOfTwo = (n) => {
+    const list = []
+    for(var i =0; i<=n; i++){
+        list[i]=Math.pow(2,i)
+    }
+    return(
+        list
+    )
+}
+
