bd2b134517d564fa73340004986466f99b798ec0
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-03-16 11:14:22 +0300

    second kata with two desicions

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 Complete the function that takes a non-negative integer n as input, 
 and returns a list of all the powers of 2 with the 
 exponent ranging from 0 to n ( inclusive ).*/
-
+/*
 const powersOfTwo = (n) => {
     const list = []
     for(var i =0; i<=n; i++){
@@ -12,5 +12,35 @@ const powersOfTwo = (n) => {
     return(
         list
     )
-}
+}*/
 
+/*The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+Here's the deal:
+It must start with a hashtag (#).
+All words must have their first letter capitalized.
+If the final result is longer than 140 chars it must return false.
+If the input or the result is an empty string it must return false.*/
+/*
+function generateHashtag (str) {
+    if(str==""){return false}
+    const arrayOfWords = str.split(' ')
+    const a=arrayOfWords.length
+    for (var i=0; i<a;i++){
+        if(arrayOfWords.indexOf('')===-1){
+            break
+        }else{
+        arrayOfWords.splice(arrayOfWords.indexOf(''),1)
+        }
+    }
+    for (var i=0; i<arrayOfWords.length; i++){
+        arrayOfWords[i]=arrayOfWords[i][0].toUpperCase() + arrayOfWords[i].slice(1)
+    }
+    let hash = '#' + arrayOfWords.join('')
+    return hash==='#'?false:hash.length<=140?hash:false
+}
+*/
+function generateHashtag(str) {
+    var hashed = '#' + str.split(' ').map(function(v) {return v.charAt(0).toUpperCase() + v.slice(1);}).join('');
+    return hashed.length > 140 || str == ""||hashed==="#" ? false : hashed;
+  }
