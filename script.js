05e1520a3d4af294abf5b262557115907da81a04
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-03-18 00:00:07 +0300

    s4

--- script.js
+++ script.js
@@ -112,3 +112,55 @@ function arrayDiff(a, b) {
     }
     return arrayDif
 }
+
+/*
+Each character have its own power: A = 1, B = 2, ... Y = 25, Z = 26
+Only capital characters can and will participate a battle.
+Only two groups to a fight.
+Group whose total power (A + B + C + ...) is bigger wins.
+If the powers are equal, it's a tie.*/
+
+function battle(x, y) {
+    const char = {
+        A:1,
+        B:2,
+        C:3,
+        D:4,
+        E:5,
+        F:6,
+        G:7,
+        H:8,
+        I:9,
+        J:10,
+        K:11,
+        L:12,
+        M:13,
+        N:14,
+        O:15,
+        P:16,
+        Q:17,
+        R:18,
+        S:19,
+        T:20,
+        U:21,
+        V:22,
+        W:23,
+        X:24,
+        Y:25,
+        Z:26
+    }
+    let xSumma = 0
+    let ySumma = 0
+    let zo = "a"
+    for (let i = 0; i < x.length; i++){
+        zo = x[i].toUpperCase()
+        xSumma = xSumma + char[zo]
+        console.log(xSumma)
+    }
+    for (let j = 0; j < x.length; j++){
+        zo = y[j].toUpperCase()  
+        ySumma = ySumma + char[zo]
+        console.log(ySumma)
+    }
+    return xSumma === ySumma ? "Tie" : xSumma > ySumma? x : y
+  }
