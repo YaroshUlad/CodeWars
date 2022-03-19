dba5b2c07436eecb4df04140df3893a66de183ad
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-03-19 10:42:59 +0300

    ss

--- script.js
+++ script.js
@@ -169,3 +169,12 @@ function battle(x, y) {
     
     return score(x) > score(y) ? x : score(x) < score(y) ? y : "Tie!"
   }
+
+ /*The goal of this exercise is to convert a string 
+ to a new string where each character in the new string 
+ is "(" if that character appears only once in the original string, or ")" 
+ if that character appears more than once in the original string.
+  Ignore capitalization when determining if a character is a duplicate. */
+
+  function duplicateEncode(word){
+}
