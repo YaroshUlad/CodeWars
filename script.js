984b977248524085cb48f59127f2bd695fbd5c0b
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-04-05 00:09:49 +0300

    sqr

--- script.js
+++ script.js
@@ -269,4 +269,10 @@ function eachCons(array, n) {
     return answer
 }
 
+/*Complete the function that calculates the area of the red square,
+    when the length of the circular arc A is given as the input.
+    Return the result rounded to two decimals.*/
 
+function squareArea(A){
+return parseFloat(Math.pow(2*A/Math.PI,2).toFixed(2))
+}
